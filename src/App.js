50e5e1e400a3667c06e34bddfe9ba6802fb4a27e
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-07 15:10:26 +0900

    feat: 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
-}
+import { Route, Routes } from "react-router-dom";
+
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+import Todo from "./pages/todo";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/todo" element={<Todo />} />
+    </Routes>
+  );
+};
 
 export default App;
