ebe34f9ca31938cdedfe74adc31c8bc7e36f9a22
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-08 12:39:48 +0900

    feat: useSignup.js 추가

---
+++ src/hook/useSignup.js
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
+import { SIGNUP_API_URL } from "../constants/api";
+import { SIGNIN } from "../constants/routes";
+
+const useSignup = () => {
+  const navigate = useNavigate();
+
+  const signup = async (email, password) => {
+    try {
+      const response = await fetch(SIGNUP_API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      if (response.status === 201) {
+        navigate(SIGNIN);
+      } else {
+        const { message } = await response.json();
+        throw new Error(`회원가입 실패 (${message})`);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return signup;
+};
+
+export default useSignup;
