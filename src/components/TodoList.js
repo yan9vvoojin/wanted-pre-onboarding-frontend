5eb9ad5474935b5b905158b26eaaea71ae9026c5
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-09 11:38:03 +0900

    style: TodoList 스타일 수정

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import useGetTodoList from "../hook/useGetTodoList";
 import deleteTodo from "../utils/deleteTodo";
 import updateTodo from "../utils/updateTodo";
@@ -7,22 +6,18 @@ import StyledTodoList from "../styles/TodoList";
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 
-const Container = styled.div`
-  width: 100%;
-`;
-
 const TodoList = () => {
   const [todoList, setTodoList] = useGetTodoList();
 
   return (
-    <Container>
+    <>
       <TodoInput setTodoList={setTodoList} />
       <StyledTodoList>
         {todoList.map((todo) => (
           <TodoItem key={todo.id} {...todo} setTodoList={setTodoList} deleteTodo={deleteTodo} updateTodo={updateTodo} />
         ))}
       </StyledTodoList>
-    </Container>
+    </>
   );
 };
 
