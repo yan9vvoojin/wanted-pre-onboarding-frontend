41d95a62e266f34cf04ac3c7a085069ceae727cd
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-09 12:46:16 +0900

    refactor: updateTodo 함수 매개변수 수정

--- src/utils/updateTodo.js
+++ src/utils/updateTodo.js
@@ -1,18 +1,18 @@
 import { TODO_API_URL } from "../constants/api";
 
-const updateTodo = async (todo, setTodoList) => {
+const updateTodo = async ({ id, todo, isCompleted }, setTodoList) => {
   try {
-    const response = await fetch(`${TODO_API_URL}/${todo.id}`, {
+    const response = await fetch(`${TODO_API_URL}/${id}`, {
       method: "PUT",
       headers: {
         Authorization: "Bearer " + localStorage.getItem("access_token"),
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ todo: todo.todo, isCompleted: todo.isCompleted }),
+      body: JSON.stringify({ todo, isCompleted }),
     });
     if (response.status === 200) {
       const newTodo = await response.json();
-      setTodoList((prevTodo) => prevTodo.map((todo) => (todo.id === newTodo.id ? newTodo : todo)));
+      setTodoList((prev) => prev.map((todo) => (todo.id === newTodo.id ? newTodo : todo)));
     } else {
       const { message } = await response.json();
       throw new Error(`할일 수정 실패 (${message})`);
