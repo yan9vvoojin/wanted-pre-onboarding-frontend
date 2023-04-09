6c3b9d8b4b1782fd479f3bcae35ee6ef2a81dfe2
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-09 12:22:55 +0900

    refactor: 비로그인 시 투두 리스트 불러오지 않게 수정

--- src/hook/useGetTodoList.js
+++ src/hook/useGetTodoList.js
@@ -6,6 +6,8 @@ const useGetTodoList = () => {
   const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
+    const access_token = localStorage.getItem("access_token");
+    if (!access_token) return;
     getTodoList();
   }, []);
 
