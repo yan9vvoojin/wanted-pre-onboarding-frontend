e3abd0683ccbdc434ee420090927c202ab2ed8f4
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-09 11:59:38 +0900

    refactor: 홈 스타일 분리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,10 @@
-import styled from "styled-components";
-
-const Container = styled.main`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 1rem;
-`;
+import HomePage from "../styles/HomePage";
 
 const Home = () => {
   return (
-    <Container>
+    <HomePage>
       <span>원티드 프리온보딩 프론트엔드 - 선발 과제</span>
-    </Container>
+    </HomePage>
   );
 };
 
---
+++ src/styles/HomePage.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HomePage = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem;
+`;
+
+export default HomePage;
