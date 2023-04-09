35402bed231980c1a4421d5d24e31e01e5ba635b
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-09 11:46:39 +0900

    refactor: api.js 변수명 변경

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
-const API_URL = "https://www.pre-onboarding-selection-task.shop";
+const BASE_API_URL = "https://www.pre-onboarding-selection-task.shop";
 
-export const SIGNIN_API_URL = `${API_URL}/auth/signin`;
-export const SIGNUP_API_URL = `${API_URL}/auth/signup`;
-export const TODO_API_URL = `${API_URL}/todos`;
+export const SIGNIN_API_URL = `${BASE_API_URL}/auth/signin`;
+export const SIGNUP_API_URL = `${BASE_API_URL}/auth/signup`;
+export const TODO_API_URL = `${BASE_API_URL}/todos`;
