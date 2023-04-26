37ac17fc798dc2930ead27e8d82f89d249942dc7
yan9vvoojin <yan9vvoojin@naver.com>
2023-04-26 21:38:44 +0900

    refactor: API_URL 상수 수정

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,4 @@
 const BASE_API_URL = "https://www.pre-onboarding-selection-task.shop";
 
-export const SIGNIN_API_URL = `${BASE_API_URL}/auth/signin`;
-export const SIGNUP_API_URL = `${BASE_API_URL}/auth/signup`;
+export const SIGN_API_URL = `${BASE_API_URL}/auth`;
 export const TODO_API_URL = `${BASE_API_URL}/todos`;
